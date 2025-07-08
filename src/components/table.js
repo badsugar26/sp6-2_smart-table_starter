f410e92ba94a07721476e07d05e2039048c3045c
badsugar26 <badsugar26@yandex.ru>
2025-07-08 20:16:49 +0700

    Проектная №6.2 Версия 1

--- src/components/table.js
+++ src/components/table.js
@@ -7,17 +7,51 @@ import {cloneTemplate} from "../lib/utils.js";
  * @param {(action: HTMLButtonElement | undefined) => void} onAction
  * @returns {{container: Node, elements: *, render: render}}
  */
+
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(templateId => {
+        const clonedTamplate = cloneTemplate(templateId);
+        root.container.prepend(clonedTamplate);
+        root[templateId] = clonedTamplate;
+    });
+
+    after.forEach(templateId => {
+        const clonedTamplate = cloneTemplate(templateId);
+        root.container.append(clonedTamplate);
+        root[templateId] = clonedTamplate;
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => {
+        onAction();
+    });
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction, 100);
+    });
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    })
+
+
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+            return row;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
